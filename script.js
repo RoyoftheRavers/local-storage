72077066f811afecc2109cdff8aff30ccfd93468
john00neill <john2984@live.com>
2020-09-16 16:20:26 +0000

    test

---
+++ script.js
@@ -0,0 +1,7 @@
+// Define variables for input
+/*let storageInput = document.querySelector('.storage');
+let text = document.querySelector('.text'); 
+let button = document.querySelector('.button');
+*/
+
+localStorage.setItem('match1', 'Man United');
